4ba3fed0de108dba9f7601c2a2efde3ee2871b98
Hamza Zaman <141897400+Hamzazaman999@users.noreply.github.com>
2023-12-27 04:28:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,113 @@
-import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { auth, db } from './config.js';
 import { collection, addDoc, getDocs, query, where, Timestamp, orderBy, doc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
-const addbtn = document.querySelector("#addbtn");
-
-onAuthStateChanged(auth, (user) => {
+let arr = [];
+let userUid;
+onAuthStateChanged(auth, async(user) => {
     if (user) {
-        const uid = user;
+        const uid = user.uid;
         console.log(uid);
+        userUid = uid;
+        const q = query(collection(db, "todos"), where("uid", "==", uid), orderBy("timestamp", "desc"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          arr.push({...doc.data(), docId: doc.id });
+        });
+        renderTodo()
     } else {
         window.location = 'login.html'
     }
-});
+  });
 
-addbtn.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    try {
-      const docRef = await addDoc(collection(db, "todos"), {
-        title: title.value,
-        description: desc.value,
-        uid: auth.currentUser.uid,
-        timestamp: Timestamp.fromDate(new Date()),
-      });
-      console.log("Document written with ID: ", docRef.id);
-      arr.unshift({
-        title: title.value,
-        description: desc.value,
-        uid: userUid,
-        docId:docRef.id
-      })
-      renderTodo()
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const desc = document.querySelector('#description');
+const div = document.querySelector('.container');
+
+
+function renderTodo() {
+  div.innerHTML = ''
+  console.log(arr);
+  arr.map((item) => {
+    div.innerHTML += `
+    <h3>Title: ${item.title}</h3>
+    <h3>Description: ${item.description}</h3>
+    
+    <button id="deleteBtn"><i  class="fa-solid fa-trash"></i></button>
+    <button id="updateBtn"><i class="fa-solid fa-pen-to-square"></i></button>
+    <hr/>
+    `
   })
+
+  const deleteBtn = document.querySelectorAll('#deleteBtn');
+  deleteBtn.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      deleteTodo(index);
+    })
+  })
+
+  //update todo
+  const updateBtn = document.querySelectorAll('#updateBtn');
+  updateBtn.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      console.log('update called', index);
+      updateTodo(index);
+    })
+  })
+}
+
+//add data
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  try {
+    const docRef = await addDoc(collection(db, "todos"), {
+      uid: auth.currentUser.uid, 
+      timestamp: Timestamp.fromDate(new Date()),
+      title: title.value,
+      description: desc.value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    arr.unshift({
+      uid: userUid,
+      docId:docRef.id,
+      title: title.value,
+      description: desc.value,
+    })
+    renderTodo()
+    title.value = '';
+    desc.value = '';
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+})
   
+function deleteTodo(index) {
+  deleteDoc(doc(db, "todos", arr[index].docId))
+    .then(() => {
+      arr.splice(index, 1);
+      renderTodo()
+    }).catch((err) => {
+      console.log(err);
+    })
+}
+function updateTodo(index) {
+  const title = prompt('enter title to update');
+  const todosRef = doc(db, "todos", arr[index].docId);
+  
+  updateDoc(todosRef, {
+    title: title
+  }).then(()=>{
+    arr[index].title = title;
+    renderTodo()
+  }).catch((err)=>{
+    console.log(err);
+  })
+
+  renderTodo()
+}
+
 
 const btn = document.querySelector('#logoutbtn')
 btn.addEventListener('click', () => {
@@ -45,6 +117,6 @@ btn.addEventListener('click', () => {
     }).catch((error) => {
         console.log('error ===>' , error);
     });
-})
+});
 
 
